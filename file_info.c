40c2fd8181abbc211baeba91821e644ecbaed4de
Jennifer Zhang <jennifer.newyork@aol.com>
2017-11-01 21:54:04 -0400

    worked on printing out size based on bytes

--- file_info.c
+++ file_info.c
@@ -9,8 +9,24 @@ void my_stat(char * file_name){
   int fd = open(file_name, O_RDONLY);
   struct stat sb;
   stat(file_name, &sb);
-  printf("File size: %ld\n", sb.st_size);
-  printf("Permissions: %ld\n", sb.st_mode);
+
+  size_t size = sb.st_size;
+  printf("File size: %lu\n", size);
+  int gb = size / (1024 * 1024 * 1024); 
+  size = size % (1024 * 1024 * 1024);
+  int mb = size / (1024 * 1024);
+  size = size % (1024 * 1024);
+  int kb = size / 1024;
+  size = size % 1024;
+  int b = size;
+
+  char str[256];
+  sprintf(str, "File size: %dGB, %dMB, %dKB, %dB", gb, mb, kb, b);
+  puts(str);
+
+  mode_t permis = sb.st_mode;
+  printf("Permissions: %o\n", permis);
+  
   printf("Time of last access: %s\n", ctime(&(sb.st_atime)));
 }
 int main(){
